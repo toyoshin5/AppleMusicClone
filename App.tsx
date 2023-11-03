1d77b6789c1591482dceb26dce8c669ead7956c6
Shingo TOYODA <toyodasinngo@gmail.com>
2023-11-03 14:34:49 +0900

    アルバム画面完成

--- App.tsx
+++ App.tsx
@@ -8,7 +8,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 //アイコン
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 import React from 'react';
 
 import {
@@ -56,8 +57,12 @@ const MusicScreen = () => {
         <ButtonArea />
         <Divider style={{ marginLeft: 20, marginTop: 4 }} />
         <SongsArea />
+        <Footer />
       </ScrollView>
-    </View>
+      <FloatingArea />
+
+    </View >
+
   )
 }
 
@@ -78,17 +83,17 @@ const TitleArea = () => {
         fontWeight: '500',
         paddingBottom: 2,
       }}
-      >Ordinary Songs 3 - EP</Text>
+      >List of Songs - EP</Text>
       <Text style={{
         fontSize: 20,
         color: colors.primary,
         paddingBottom: 4,
-      }}>Snail's House</Text>
+      }}>Toyoda Shingo</Text>
       <Text style={{
         fontSize: 12,
         color: colors.gray,
         fontWeight: '600',
-      }}>Electronic・2017年・ロスレス</Text>
+      }}>Electronic・2023年・ロスレス</Text>
     </View>
   )
 }
@@ -102,15 +107,25 @@ const ButtonArea = () => {
         flexDirection: 'row',
 
       }}>
-      <AppButton onPress={() => { }} title="再生" />
+      <AppButton onPress={() => { }} title="再生" iconName='play' />
       <View style={{ width: 16 }} />
-      <AppButton onPress={() => { }} title="シャッフル" />
+      <AppButton onPress={() => { }} title="シャッフル" iconName='shuffle' />
     </View>
   )
 }
 
 const SongsArea = () => {
-  var songs: String[] = ["Good Day", "Bouquet", "Aloha", "あめあがりのうた", "Good Day", "Bouquet", "Aloha", "あめあがりのうた", "Good Day", "Bouquet", "Aloha", "あめあがりのうた", "Good Day", "Bouquet", "Aloha", "あめあがりのうた"]
+  var songs: String[] = [
+    "Good Day", "Bouquet", "Aloha", "Sunrise Serenade", "Moonlit Melody",
+    "Ocean Breeze", "Eternal Echoes", "Whispering Wind", "Mystic Mirage",
+    "Golden Horizon", "Enchanted Evening", "Starlight Serenade",
+    "Lullaby of the Stars", "Celestial Harmony", "Serenity's Embrace",
+    "Dancing Fireflies", "Melody of the Mountains", "Hidden Oasis",
+    "Radiant Reverie", "Midnight Mirage", "Journey to Infinity",
+    "Crystal Cascade", "Soothing Symphony", "Echoes of Eternity",
+    "Harmony Haven", "Serendipity Sonata", "Velvet Twilight",
+    "Tranquil Tides", "Whimsical Waltz", "Chasing Dreams"
+  ]
   return (
     <View style={{
       paddingHorizontal: 20,
@@ -135,20 +150,77 @@ const SongItem = ({ number, name, onPress }: { number: String; name: String; onP
     }}>
       <Text style={styles.songNumText}>{number}</Text>
       <Text style={styles.songItemText}>{name}</Text>
-      <Icon name="home" size={10} />
+      <EntypoIcon name="dots-three-horizontal" size={16} style={{ marginLeft: 'auto', marginRight: 0 }} />
     </TouchableOpacity><Divider style={{ marginLeft: 24 }} /></>
   )
 }
 
-const AppButton = ({ onPress, title }: { onPress: () => void; title: string; }) => {
+const AppButton = ({ onPress, title, iconName }: { onPress: () => void; title: string; iconName: string }) => {
   return (
     <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Ionicons name={iconName} size={24} style={{ marginRight: 6, color: colors.primary }} />
       <Text style={styles.buttonText}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
+const FloatingArea = () => {
+  return (
+    <View style={styles.floatView}>
+      <TouchableOpacity style={styles.floatButton}>
+        <Image
+          source={require('./img/ordinary_songs.jpg')} // 画像のパスを指定
+          style={styles.floatimage} />
+        <Text style={styles.floatTitle}>Good Day</Text>
+        <Ionicons name="play" size={24} style={{ marginRight: 24, marginLeft: "auto" }} />
+        <Ionicons name="play-forward" size={26} style={{ marginRight: 8, color: "#b3b3b3" }} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Footer = () => {
+  return (
+    <View style={{ margin: 20, marginBottom: 200 }}>
+      <Text style={{ color: colors.gray }}>
+        2023年11月3日{"\n"}30曲、300分
+      </Text>
+
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
+  floatTitle: {
+    fontSize: 16,
+    paddingLeft: 8,
+  },
+  floatimage: {
+    height: 42,
+    width: 42,
+    borderRadius: 8, // containerと同じ値を設定
+  },
+  floatView: {
+    position: 'absolute',
+    left: 12,
+    right: 12,
+    bottom: 60, // ボタンの高さを調整するために必要に応じて調整
+    alignSelf: 'center',
+    elevation: 5, // ボタンを浮かせる効果
+  },
+  floatButton: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 56,
+    borderRadius: 12,
+    backgroundColor: "#fbfbfb",
+    alignItems: 'center',
+    shadowColor: '#999',
+    paddingHorizontal: 8,
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.7,
+    shadowRadius: 20,
+  },
   songNumText: {
     color: colors.gray,
     fontSize: 16,
